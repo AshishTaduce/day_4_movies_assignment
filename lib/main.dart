6b6864013bdfc8ae9a7cd8473e251e0474052e5a
AshishTaduce <ashishteddy2@gmail.com>
2019-10-04 22:53:20 +0530

    Carousal Working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:day_4_movies_assignment/movies.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:carousel_slider/carousel_slider.dart';
@@ -6,85 +7,70 @@ import 'package:carousel_slider/carousel_slider.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.black ,
+      backgroundColor: Colors.black,
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
           'Movies Poster',
-          style: TextStyle(color: Colors.black),
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.black,),
         ),
         backgroundColor: Colors.yellow,
       ),
-      body: MoviesPage(),// Use stateful widget you created here
+      body: MoviesPage(), // Use stateful widget you created here
     ),
   ));
-
 }
 
-// You are given json string of movies list (see file movies.dart)
 String movies = MoviesList.moviesJsonList;
 List movieList = json.decode(movies);
-
-// Hint:
-// Convert the string to List of maps using jsonDecode and then use it
-
-// Create a stateful widget called MoviesPage here
 
 class MoviesPage extends StatefulWidget {
   @override
   _MoviesPageState createState() => _MoviesPageState();
 }
-class _MoviesPageState extends State<MoviesPage> {
 
+class _MoviesPageState extends State<MoviesPage> {
   Map movie1 = movieList[0];
   int index = 0;
-  List id =[];
+  List id = [];
   List poster = [];
-  String imageurl = "https://raw.githubusercontent.com/android10/Sample-Data/master/Android-CleanArchitecture-Kotlin/posters/038001.jpg";
   @override
   Widget build(BuildContext context) {
-    String moviesList = MoviesList.moviesJsonList;
-    for(var x in movieList){
+    for (var x in movieList) {
       poster.add(x['poster']);
       id.add(x['id']);
     }
-
-
-
-
     return Container(
-        child : Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                flex: 9,
-                child: Image.network('${poster [index]}',),),
-              Expanded(
-                flex: 1,
-                child: RaisedButton(
-                  color: Colors.yellow,
-                  textColor: Colors.black,
-                  child: const Text(
-                      'Next Movie',
-                      style: TextStyle(fontSize: 20)
-                  ),
-                  onPressed: (){
-                    setState(() {
-                      index++;
-                      print(index);
-                     if (index == poster.length -1){index = 0;}
-                    });
-                  },
+      color: Colors.yellow,
+        child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CarouselSlider(
+                  height: 400.0,
+                  items: new List<int>.generate(
+                    poster.length - 1,
+                    (i) => i + 1,
+                  ).map((i) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            //decoration: BoxDecoration(color: Colors.amber),
+                            child: Image.network(
+                              '${poster[i]}',
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ).toList(),
                 ),
-              )
-            ],
-          ),
-        )
-    );
+              ],
+            )));
   }
 }
-
-
