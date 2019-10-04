abfd0a1f129675dc2e3a35182af4aed085a21736
AshishTaduce <ashishteddy2@gmail.com>
2019-10-04 14:14:15 +0530

    Need help

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:day_4_movies_assignment/movies.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -13,16 +14,63 @@ void main() {
         ),
         backgroundColor: Colors.yellow,
       ),
-      body: Container(),// Use stateful widget you created here
+      body: MoviesPage(),// Use stateful widget you created here
     ),
   ));
+
 }
 
 // You are given json string of movies list (see file movies.dart)
-String moviesList = MoviesList.moviesJsonList;
+String movies = MoviesList.moviesJsonList;
+List movieList = json.decode(movies);
+
+
 
 // Hint:
 // Convert the string to List of maps using jsonDecode and then use it
 
 // Create a stateful widget called MoviesPage here
+
+class MoviesPage extends StatefulWidget {
+  @override
+  _MoviesPageState createState() => _MoviesPageState();
+}
+
+class _MoviesPageState extends State<MoviesPage> {
+
+  Map movie1 = movieList[0];
+
+
+
+  List id = movieList.toList();
+  List poster = [];
+  @override
+  Widget build(BuildContext context) {
+    for(var x in movieList){
+      poster.add(x['poster']);
+      id.add(x['id']);
+    }
+
+
+    return Container(
+        child : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            //Image.network('$imageurl'),
+            Image.network(poster[5]),
+            RaisedButton(
+              color: Colors.yellow,
+              textColor: Colors.black,
+              child: const Text(
+                  'Next Movie',
+                  style: TextStyle(fontSize: 20)
+              ),
+            )
+          ],
+        )
+    );
+  }
+}
+
 
